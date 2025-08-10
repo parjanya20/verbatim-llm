71743d3376c3d18394d0cd17a214edca4340a9dd
parjanya20 <parjanya20@gmail.com>
2025-08-09 18:49:04 -0700

    Update: Add metrics and allow different tokenizers

--- verbatim_llm/tokenswap.py
+++ verbatim_llm/tokenswap.py
@@ -10,22 +10,32 @@ class TokenSwapProcessor(LogitsProcessor):
     This is a logit processor which implements tokenswap
     """
     
-    def __init__(self, aux_model, tokenizer, grammar_words = None):
+    def __init__(self, aux_model, tokenizer, grammar_words=None, aux_tokenizer=None):
         """
         Initialize TokenSwap processor.
         
         Args:
             aux_model: Auxiliary model
+            tokenizer: Main model tokenizer
             grammar_words: List of grammar words to apply swapping to
-            tokenizer: Tokenizer
+            aux_tokenizer: Auxiliary model tokenizer (optional)
         """
         self.aux_model = aux_model
         self.tokenizer = tokenizer
+        self.aux_tokenizer = aux_tokenizer
         self.aux_past_kv = None
         self.first_call = True
-
+        
+        # Check if tokenizers are different
+        self.tokenizers_different = (
+            aux_tokenizer is not None and 
+            tokenizer.get_vocab() != aux_tokenizer.get_vocab()
+        )
+        
         if grammar_words is None:
             grammar_words = default_grammar_words
+            
+        # Get main model grammar IDs
         self.grammar_ids = torch.tensor([
             tokenizer.encode(word, add_special_tokens=False)[0] 
             for word in grammar_words
@@ -34,32 +44,53 @@ class TokenSwapProcessor(LogitsProcessor):
         device = next(aux_model.parameters()).device
         self.grammar_ids = self.grammar_ids.to(device)
         
-    def __call__(self, input_ids, scores):
-
-        with torch.no_grad():
-            if self.first_call:
-                aux_outputs = self.aux_model(input_ids, use_cache=True)
-                self.first_call = False
+        if aux_tokenizer is not None:
+            if self.tokenizers_different:
+                aux_grammar_ids = torch.tensor([
+                    aux_tokenizer.encode(word, add_special_tokens=False)[0] 
+                    for word in grammar_words
+                ])
+                self.aux_grammar_ids = aux_grammar_ids.to(device)
             else:
-                aux_outputs = self.aux_model(
-                    input_ids[:, -1:], 
-                    past_key_values=self.aux_past_kv, 
-                    use_cache=True
-                )
-            
-            self.aux_past_kv = aux_outputs.past_key_values
+                self.aux_grammar_ids = self.grammar_ids
+        else:
+            self.aux_grammar_ids = self.grammar_ids
+        
+    def __call__(self, input_ids, scores):
+        with torch.no_grad():
+            if self.tokenizers_different:
+                text = self.tokenizer.decode(input_ids[0], skip_special_tokens=True)
+                aux_input_ids = self.aux_tokenizer.encode(text, return_tensors="pt", add_special_tokens=True)
+                aux_input_ids = aux_input_ids.to(input_ids.device)
+                
+                aux_outputs = self.aux_model(aux_input_ids, use_cache=False)
+            else:
+                if self.first_call:
+                    aux_outputs = self.aux_model(input_ids, use_cache=True)
+                    self.first_call = False
+                else:
+                    aux_outputs = self.aux_model(
+                        input_ids[:, -1:], 
+                        past_key_values=self.aux_past_kv, 
+                        use_cache=True
+                    )
+                
+                self.aux_past_kv = aux_outputs.past_key_values
             
             p_main = torch.softmax(scores, dim=-1)
             p_aux = torch.softmax(aux_outputs.logits[:, -1], dim=-1)
             
             p_main_sum = p_main[:, self.grammar_ids].sum(dim=-1, keepdim=True)
-            p_aux_sum = p_aux[:, self.grammar_ids].sum(dim=-1, keepdim=True)
+            p_aux_sum = p_aux[:, self.aux_grammar_ids].sum(dim=-1, keepdim=True)
             
             p_gen = p_main.clone()
             mask = p_aux_sum > 0
             if mask.any():
                 ratio = p_main_sum / p_aux_sum.clamp(min=1e-10)
-                p_gen[:, self.grammar_ids] = ratio * p_aux[:, self.grammar_ids]
+                aux_probs_mapped = torch.zeros_like(p_main)
+                aux_probs_mapped[:, self.grammar_ids] = p_aux[:, self.aux_grammar_ids]
+                
+                p_gen[:, self.grammar_ids] = ratio * aux_probs_mapped[:, self.grammar_ids]
                 p_gen = p_gen / p_gen.sum(dim=-1, keepdim=True)
             
             return torch.log(p_gen.clamp(min=1e-10))
